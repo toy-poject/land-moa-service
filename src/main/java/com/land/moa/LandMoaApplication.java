808d3e6334693effc42b8df0aeb9ae243f15b9ab
wlsgud4616 <wlsgud4616@naver.com>
2022-02-26 15:02:59 +0900

    Initial commit

---
+++ src/main/java/com/land/moa/LandMoaApplication.java
@@ -0,0 +1,13 @@
+package com.land.moa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandMoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandMoaApplication.class, args);
+	}
+
+}
